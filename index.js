7d48c68841897a746dd19d1bee1dfb81b8e0e455
LePichu <ishat.prsnl65844@gmail.com>
2021-08-04 19:12:52 +0530

    i have no idea wtf am i doing

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-import "vue"
-
 console.log("Loaded!")
 const vueTest = new Vue(
     {
         el: "#website",
         data: {
-            message: "Hello World!"
+            message: "Markdown Test Below:" 
         }
     }
 )
