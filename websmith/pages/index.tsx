83588332201aa82e4ec7fef60188e728e5563140
IshanKBG <jaiswalishan701@gmail.com>
2021-10-28 22:14:01 +0530

    feat: added websmith website src

---
+++ websmith/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+
+const Home: NextPage = () => {
+  return (
+  <>
+  <Head>
+    <title>Websmith</title>
+  </Head>
+  <h1>Pichu is 🐭</h1>
+  </>
+  )
+}
+
+export default Home
